3cb7cb413ee6379d906e34198c9cb84da235a4c4
WILLIES <WILLIES@WILLIES-PC>
2018-06-29 17:12:40 +1200

    Simple menu - Ability to retrieve definition of user input - Ability to retrieve definition of a randomly selected word

--- DictionaryTestApp/Program.cs
+++ DictionaryTestApp/Program.cs
@@ -12,17 +12,52 @@ namespace DictionaryTestApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string[] words;
 
+        static void Main(string[] args)
         {
             Initiate();
         }
 
         static void Initiate()
         {
+            // Retrieve word list
+            words = File.ReadAllLines(@"C:\Users\WILLIES\Desktop\words.txt");
+
+            // Simple Menu
+            Console.WriteLine("0- Definition by typed word \n1- Definition by random word");
+            int character = Convert.ToInt16(Console.ReadLine());
+
+            switch (character)
+            {
+                case 0:
+                    DefinitionByWord();
+                    break;
+
+                case 1:
+                    DefinitionByRandomWord();
+                    break;
+            }
+        }
+
+        static void DefinitionByWord()
+        {
+            Console.WriteLine("");
             Console.WriteLine("Type in a word to define");
             string wordToDefine = Console.ReadLine();
             GetDefinitionByRootObjectAsync(wordToDefine).Wait();
+            Initiate();
+        }
+
+        static void DefinitionByRandomWord()
+        {
+            Random r = new Random();
+            int randomNumber = r.Next(0, 400000);
+
+            Console.WriteLine("");
+            Console.WriteLine(string.Format("Definition of {0}:", words[randomNumber]));
+            GetDefinitionByRootObjectAsync(words[randomNumber]).Wait();
+            Initiate();
         }
 
         static async Task GetDefinitionByRootObjectAsync(string prWord)
@@ -38,13 +73,11 @@ namespace DictionaryTestApp
                     Console.WriteLine(string.Format("- {0}", currentResult.results[0].lexicalEntries[0].entries[0].senses[i].definitions[0]));
                 }
                 Console.WriteLine("");
-                Initiate();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("");
-                Initiate();
             }
         }
     }
